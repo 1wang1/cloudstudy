ee4b28a9b93c92c34abab9256772bdb0f2dc7122
WangZhenkun <zhenkun.wang@foxmail.com>
2021-09-23 15:20:36 +0800

    初始化eureka-client

--- eureka-server-1/src/main/java/com/wangsan/eurekaserver1/EurekaServer1Application.java
+++ eureka-server-1/src/main/java/com/wangsan/eurekaserver1/EurekaServer1Application.java
@@ -2,12 +2,15 @@ package com.wangsan.eurekaserver1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer1Application.class, args);
+        System.out.println("EurekaServer1 success start");
     }
 
 }
