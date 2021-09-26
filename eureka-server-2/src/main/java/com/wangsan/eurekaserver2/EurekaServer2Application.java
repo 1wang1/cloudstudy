d4245dd45f3012f512a4fe52be6a9b7d3335b261
WangZhenkun <zhenkun.wang@foxmail.com>
2021-09-26 11:16:16 +0800

    eurekaServer集群配置

--- eureka-server-2/src/main/java/com/wangsan/eurekaserver2/EurekaServer2Application.java
+++ eureka-server-2/src/main/java/com/wangsan/eurekaserver2/EurekaServer2Application.java
@@ -2,12 +2,15 @@ package com.wangsan.eurekaserver2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServer2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServer2Application.class, args);
+        System.out.println("eurekaserver2 success start");
     }
 
 }
